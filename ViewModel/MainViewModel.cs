a1f1ce190258db19693023bb9f62ee01a2ad3a9c
SongWei <songwei@xdu.mstechclub.cn>
2013-04-11 11:31:14 +0800

    简单的框架
    没有特别的代码，只是简单的UI框架

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErlangEditor.ViewModel
+{
+    public class MainViewModel
+    {
+        public bool Loaded
+        {
+            get;
+            private set;
+        }
+
+        public void Load()
+        {
+            Loaded = true;
+        }
+    }
+}
